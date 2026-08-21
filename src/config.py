e2ef49540e3b679f117c00cf833cf9708b3603b8
Shawky El-gendy <90721569+ShawkyEl-gendy@users.noreply.github.com>
2026-08-21 11:42:33 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-DATASET_PATH = "./data/mrc-filtered.jsonl" # mrc, ARCD ,ArQuAD,ALRAGE
+DATASET_PATH = "./data/mrc-filtered.jsonl" # mrc, ARCD 
 CHUNK_STRATEGY = "recursive" # Options:  "fixed", "recursive", "semantic"
 CHUNK_SIZE = 512
 CHUNK_OVERLAP = 50
 MIN_CHUNK_SIZE = 75
 SIMILARITY_THRESHOLD = 0.65
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
-DB_PATH = "./db/MRC" # MRC, ARCD ,ArQuAD
+DB_PATH = "./db/MRC" # MRC, ARCD 
 COLLECTION_NAME = f"mrc_collection_chunks_{CHUNK_STRATEGY}" # mrc, ARCD ,ArQuAD
 USE_NER = True
 # Generation model
