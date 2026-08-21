a8a4af943503fd3fb505ebb2b73fb4d153c9100b
Shawky El-gendy <90721569+ShawkyEl-gendy@users.noreply.github.com>
2026-08-21 11:42:57 +0200

    Fix formatting in config.py for COLLECTION_NAME

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ MIN_CHUNK_SIZE = 75
 SIMILARITY_THRESHOLD = 0.65
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
 DB_PATH = "./db/MRC" # MRC, ARCD 
-COLLECTION_NAME = f"mrc_collection_chunks_{CHUNK_STRATEGY}" # mrc, ARCD ,ArQuAD
+COLLECTION_NAME = f"mrc_collection_chunks_{CHUNK_STRATEGY}" # mrc, ARCD 
 USE_NER = True
 # Generation model
 MODEL_NAME = "aya:8b" # ["llama3:8b","aya:8b","qwen2.5:3b","command-r7b-arabic"]
